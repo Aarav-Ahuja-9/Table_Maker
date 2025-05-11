7e0fe8ed836124597604f6540a36324f1d7c099c
Aarav-Ahuja-9 <mr.aaravahuja@gmail.com>
2025-05-11 12:19:11 +0530

    Update app.js

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -1,28 +1,47 @@
 const nums = document.querySelectorAll(".number");
 const answers = document.querySelectorAll(".answer");
+
+for (let answer of answers) {
+    answer.innerText = 0;
+}
+
 let input = prompt("please enter the desired value of the table");
+
+
+for (let num of nums) {
+    num.innerText = input;
+}
 
 if (input === "" || input === null ) {
     input = prompt("Please enter a value");
-} else if (input !== "" || input !== null) {
     for (let num of nums) {
         num.innerText = input;
     }
-}
+} else if (input !== "" || input !== null) {}
 
 const calculate = () => {
-    for (i = 0; i < 15; i++) {
+    for (i = 0; i < 3; i++) {
         if (input === "" || input === null) {
             input = prompt("Please enter a value");
-            
+            for (let num of nums) {
+                num.innerText = input;
+            }
         } else if (input !== "" || input !== null) {
             let num = 1;
             for (let answer of answers) {
                 answer.innerText = input * num;
                 num ++;
+
+
             }
         }
     }
 }
 
 calculate();    
+
+for (let num of nums) {
+    if (input === "" || input === null) {
+        num.innerText = 0;
+    }
+}
